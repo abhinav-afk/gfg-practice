e85b5f388dd279a88f668f5a2843420934a8d896
abhinav <73064829+abhinav-afk@users.noreply.github.com>
2026-05-09 18:37:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check if subtree/check-if-subtree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool validate(Node* root1, Node* root2) {
+        if (!root2)
+            return true;
+        if (!root1)
+            return false;
+        if (root1->data != root2->data)
+            return false;
+        return validate(root1->left, root2->left) &&
+               validate(root1->right, root2->right);
+    }
+    bool isSubTree(Node* root1, Node* root2) {
+        if (!root1)
+            return false;
+        if (root1->data == root2->data) {
+            bool check = validate(root1, root2);
+            if (check)
+                return true;
+        }
+        return isSubTree(root1->left, root2) || isSubTree(root1->right, root2);
+    }
+};
+
